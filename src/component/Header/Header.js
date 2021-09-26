01f893c4c1748a3d948e79c3504ff281dab37a15
poojaranidey <poojaranidey@gmail.com>
2021-09-26 22:19:52 +0600

    done commit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         // header area 
         <div className="header">
             <div className="header-details">
-                <h1>Help world biggest charity by watching your favourite movies 2021</h1>
+                <h1>Help world biggest charity by watching your favourite movies 2021...</h1>
                 <p>Donate to Girls Education</p>
                 <h2>Total Donate this year: $500000</h2>
             </div>
